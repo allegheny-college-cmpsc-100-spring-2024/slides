f751b9aee73dd04dfccc7baee2014aff96febf0d
howshekilledit <mgreen@allegheny.edu>
2024-01-14 20:12:01 -0500

    add rename function partial

--- linkslides.py
+++ linkslides.py
@@ -1,15 +1,20 @@
 import os
+
+# needs more
+def rename(files):
+#if number in format 1-9, add a 0 to the front
+    if len(number) == 1:
+        number = "0" + number
+    # rename file
+    os.rename(os.path.join(folder, file), os.path.join(folder, number + "_" + file.split("_")[1]))
+
 def linkslides(folder):
     # for each numbered markdown file in the folder
-    for file in sorted(os.listdir(folder)):
+    for file in sorted(os.listdir(folder), reverse = True):
         if file.endswith(".md") and file[0].isdigit():
             # get the file number
             number = file.split("_")[0]
-            # if number in format 1-9, add a 0 to the front
-            # if len(number) == 1:
-            #     number = "0" + number
-            # # rename file
-            # os.rename(os.path.join(folder, file), os.path.join(folder, number + "_" + file.split("_")[1]))
+         
 
             # get the file title
             title = file.split("_")[1]
@@ -21,7 +26,7 @@ def linkslides(folder):
                 wfile.write(link)
 
             # get the file link
-            link = f"[next: {title}]({folder}/blob/main/{file})"
+            link = f"[next: {title}](/{file})"
             print(link)
 
 if __name__ == "__main__":
